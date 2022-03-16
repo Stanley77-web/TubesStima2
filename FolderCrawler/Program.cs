9a8dfabb51283ee0906206ef5bec3e4c45116ee0
JeremyRio <73146752+JeremyRio@users.noreply.github.com>
2022-03-16 07:39:17 +0700

    update algo BFS

--- FolderCrawler/Program.cs
+++ FolderCrawler/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FolderCrawler
@@ -11,6 +10,50 @@ namespace FolderCrawler
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        
+        static public void BFS()
+        {
+            Boolean found = false;
+            string find_file = "hehe.txt";
+            Queue<string> queue_BFS = new Queue<string>();
+            // Input path disini
+            string path = @"D:\Video Recording";
+            string[] directories = Directory.GetDirectories(path);
+            string[] files = Directory.GetFiles(path);
+            foreach (string directory in directories)
+            {
+                queue_BFS.Enqueue(directory);
+            }
+            foreach (string file in files)
+            {
+                queue_BFS.Enqueue(file);
+            }
+
+            while (queue_BFS.Count > 0 && !found)
+            {
+                string current_file = queue_BFS.Dequeue();
+                string file_name = Path.GetFileName(current_file);
+                Console.WriteLine(file_name);
+                if (file_name == find_file)
+                {
+                    found = true;
+                }
+                else if (Directory.Exists(current_file))
+                {
+                    directories = Directory.GetDirectories(current_file);
+                    files = Directory.GetFiles(current_file);
+                    foreach (string directory in directories)
+                    {
+                        queue_BFS.Enqueue(directory);
+                    }
+                    foreach (string file in files)
+                    {
+                        queue_BFS.Enqueue(file);
+                    }
+                }
+            }
+        }
+
         [STAThread]
         static void Main()
         {
