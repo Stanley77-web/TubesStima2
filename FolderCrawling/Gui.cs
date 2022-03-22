e579f47417bdd610b7158b57965b54f0e393d61c
Jeremy <73146752+JeremyRio@users.noreply.github.com>
2022-03-22 19:26:09 +0700

    Update Gui.cs

--- FolderCrawling/Gui.cs
+++ FolderCrawling/Gui.cs
@@ -9,10 +9,8 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Windows.Forms;
 
-namespace FolderCrawling
-{
-    public partial class Gui : Form
-    {
+namespace FolderCrawling {
+    public partial class Gui : Form {
         private string dirChosen = null;
         private bool hasDir = false;
         private bool hasFilename = false;
@@ -24,78 +22,59 @@ namespace FolderCrawling
         Program prog;
 
         [STAThread]
-        static void Main()
-        {
+        static void Main() {
             // inisialisasi ngentot
             ApplicationConfiguration.Initialize();
             Application.Run(new Gui());
         }
-       
 
-        public Gui()
-        {
+
+        public Gui() {
             InitializeComponent();
             radioButton2.Checked = true;
 
         }
 
-        private void dirButton_Click(object sender, EventArgs e)
-        {
+        private void dirButton_Click(object sender, EventArgs e) {
             FolderBrowserDialog fbd = new FolderBrowserDialog();
             fbd.Description = "Custom Description";
 
-            if (fbd.ShowDialog() == DialogResult.OK)
-            {
+            if (fbd.ShowDialog() == DialogResult.OK) {
                 dirChosen = fbd.SelectedPath;
                 label2.Text = dirChosen;
                 hasDir = true;
             }
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            if(textBox1.Text == "Input Filename" || textBox1.Text == "")
-            {
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+            if (textBox1.Text == "Input Filename" || textBox1.Text == "") {
                 hasFilename = false;
-            }
-            else
-            {
+            } else {
                 hasFilename = true;
             }
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-            if (findAllOccurence) { findAllOccurence = true; }
-            else { findAllOccurence = false;  }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) {
+            if (findAllOccurence) { findAllOccurence = true; } else { findAllOccurence = false; }
             checkBox1.Text = "Find All Occurence"; // temp fix anj gangerti lagi jancokkkk
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (!hasDir)
-            {
+        private void button1_Click(object sender, EventArgs e) {
+            if (!hasDir) {
                 MessageBox.Show("Folder Directory not chosen", "Error");
-            }
-            else if (!hasFilename)
-            {
+            } else if (!hasFilename) {
                 MessageBox.Show("File Name not set", "Error");
-            }
-            else
-            {
+            } else {
                 gViewer1.OutsideAreaBrush = Brushes.Silver;
                 gViewer1.PanButtonPressed = true;
                 programRunTime.Restart();
                 programRunTime.Start();
                 prog = new Program(textBox1.Text, dirChosen);
-                if (radioButton2.Checked)
-                {
-                    gViewer1.Graph = prog.BFS();
-                }
-                else if (radioButton3.Checked)
-                {
+                if (radioButton2.Checked) {
+                    gViewer1.Graph = prog.BFS(checkBox1.Checked);
+                } else if (radioButton3.Checked) {
                     prog.DFS();
-                    MessageBox.Show("jalan cok DFS tapi blm diimplementasi timot");
+                    gViewer1.Graph = prog.DFS();
                 }
                 programRunTime.Stop();
                 TimeSpan ts = programRunTime.Elapsed;
